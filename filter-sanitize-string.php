f1f6dbf0508f242cd6fd447ab2057dded6b7d593
lito pj <litopj@yahoo.com>
2016-08-30 18:59:45 +0800

    first commit

---
+++ filter-sanitize-string.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+ * Sanitize a single string or array of string 
+ *
+ * sample usage:
+ * $_POST = fss( $_POST );
+ *
+ * @param str|array $str
+ * @return str|array (it depends on the input)
+*/
+function fss( $str ){
+
+    if ( is_array( $str ) ) {
+        $ret_arr = array();
+
+        foreach ($str as $k => $v) {
+            $ret_arr[$k] = filter_var( $v, FILTER_SANITIZE_STRING );
+
+            if ( is_array($v) ) {
+                $ret_arr[$k] = fss( $v );
+            }
+        }
+
+        return $ret_arr;
+            
+    }else{
+        $str = trim($str);
+        return filter_var( $str, FILTER_SANITIZE_STRING );
+    }
+}
